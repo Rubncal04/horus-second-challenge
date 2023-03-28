07dd4ea0c79c7cb2abafa47eaa94c4fb59a574a9
Ruben Gomez <rubncal.9501@gmail.com>
2023-03-27 20:28:41 -0500

    Deploy app on fly.io

--- config.js
+++ config.js
@@ -4,10 +4,10 @@ config()
 
 module.exports = {
   db: {
-    user: process.env.USER_DB,
-    password: process.env.PASSWORD_DB,
-    host: process.env.HOST_DB,
-    port: process.env.PORT_DB,
-    database: process.env.DATABASE
+    user: process.env.USER_DB || process.env.Username,
+    password: process.env.PASSWORD_DB || process.env.Password,
+    host: process.env.HOST_DB || process.env.Hostname,
+    port: process.env.PORT_DB || process.env.Postgresport,
+    database: process.env.DATABASE || process.env.database
   }
 }
--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ app.use("/graphql", graphqlHTTP({
   }
 }))
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT)
 
 console.log('Server running in port: ' + PORT);
